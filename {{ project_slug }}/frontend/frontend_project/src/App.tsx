a6e2e43fd1107c3afbf51797aafadc706a00739f
Ioannis Ts <ai.research1@arifoods.gr>
2025-07-29 13:12:48 +0300

    reformed

--- {{ project_slug }}/frontend/frontend_project/src/App.tsx
+++ {{ project_slug }}/frontend/frontend_project/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 text-white flex flex-col items-center justify-center p-8">
@@ -21,4 +21,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
